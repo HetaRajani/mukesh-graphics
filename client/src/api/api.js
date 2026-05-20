08a3a6bc763c9cb10e6daa6bd055ebcfcb1293bd
Wellness Haven Team <dev@wellnesshaven.com>
2026-05-20 21:54:13 +0530

    Force Vercel rebuild

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -4,4 +4,9 @@ const API = axios.create({
   baseURL: "https://printcraft-express.onrender.com/api",
 });
 
+console.log(
+  "API BASE URL:",
+  "https://printcraft-express.onrender.com/api"
+);
+
 export default API;
